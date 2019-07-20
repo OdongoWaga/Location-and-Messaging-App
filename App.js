b81687334df977a36118d6371dd13e305739f304
Waga <wagaodongo@gmail.com>
2019-07-20 17:25:40 +0300

    Status Bar

--- App.js
+++ App.js
@@ -1,58 +1,55 @@
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component{
-
+export default class App extends React.Component {
   renderMessageList() {
     return (
       <View style={styles.content}></View>
-    )
+    );
   }
 
   renderInputMethodEditor() {
     return (
-      <View styles={styles.renderInputMethodEditor}></View>
-    )
+      <View style={styles.inputMethodEditor}></View>
+    );
   }
 
   renderToolbar() {
     return (
-      <View style={styles.toolbar}> </View>
-    )
+      <View style={styles.toolbar}></View>
+    );
   }
 
   render() {
     return (
-      <View style={styles.container}> 
-      {this.renderMessageList()}
-      {this.renderToolbar()}
-      {this.renderInputMethodEditor()}
+      <View style={styles.container}>
+        {this.renderMessageList()}
+        {this.renderToolbar()}
+        {this.renderInputMethodEditor()}
       </View>
-    )
-    
 
-  }
+);
+}
 }
 
-const styles =StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'white',
-  }, 
-  content:{
-    flex:1,
-    backgroundColor: '#fff'
-  },
-  renderInputMethodEditor:{
-    flex:1,
-    backgroundColor: '#fff'
-  },
-  toolbar: {
-    borderTopWidth: 1,
-    borderTopColor: 'rgba(0,0,0, 0.04)'
-  },
-
-
-})
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: 'white',
+},
+content: {
+flex: 1,
+backgroundColor: 'white',
+},
+inputMethodEditor: {
+flex: 1,
+backgroundColor: 'white',
+},
+toolbar: {
+borderTopWidth: 1,
+borderTopColor: 'rgba(0,0,0,0.04)',
+backgroundColor: 'white',
+},
+});
 
 
