4181fa9ac72c1d6e94f26557a97d6d7ceff198e4
Waga <wagaodongo@gmail.com>
2019-07-20 16:55:50 +0300

    App Changes

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component{
+
+  renderMessageList() {
+    return (
+      <View style={styles.content}></View>
+    )
+  }
+
+  renderInputMethodEditor() {
+    return (
+      <View styles={styles.renderInputMethodEditor}></View>
+    )
+  }
+
+  renderToolbar() {
+    return (
+      <View style={styles.toolbar}> </View>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}> 
+      {this.renderMessageList()}
+      {this.renderToolbar()}
+      {this.renderInputMethodEditor()}
+      </View>
+    )
+    
+
+  }
 }
 
-const styles = StyleSheet.create({
+const styles =StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'white',
+  }, 
+  content:{
+    flex:1,
+    backgroundColor: '#fff'
   },
-});
+  renderInputMethodEditor:{
+    flex:1,
+    backgroundColor: '#fff'
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0, 0.04)'
+  },
+
+
+})
+
+
